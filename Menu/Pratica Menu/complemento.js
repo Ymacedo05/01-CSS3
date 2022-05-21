69fbfab46893033a8bcd33554db27b8e5fee22eb
YM <ymacedo05@gmail.com>
2022-05-20 23:21:53 -0300

    ...
    ...

---
+++ Menu/Pratica Menu/complemento.js
@@ -0,0 +1,19 @@
+function click01(){
+    var btn = document.getElementById("btn1")
+    var btn2 = document.getElementById("btn2")
+    var header01 = document.getElementById("header01")
+    var header02 = document.getElementById("header02")
+
+    header01.setAttribute("id","header02")
+}
+
+function click02(){
+    var btn = document.getElementById("btn1")
+    var btn2 = document.getElementById("btn2")
+    var header01 = document.getElementById("header01")
+    var header02 = document.getElementById("header02")
+
+    header02.setAttribute("id","header01")
+    
+}
+
